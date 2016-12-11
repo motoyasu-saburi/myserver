a87db78e87795358729d0a3f7f6d295458733bd0
kumagoro <kumagoro@MP.local>
2016-12-11 19:00:24 +0900

    add router process

--- server.go
+++ server.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+  "strings"
   "fmt"
   "net"
   "unicode/utf8"
   "strconv"
+  "bufio"
+  "os"
 )
+
+
 
 func main() {
   listener, err := net.Listen("tcp", "localhost:8080")
@@ -14,7 +19,6 @@ func main() {
     return
   }
   defer listener.Close()
-
   conn, err := listener.Accept()
   if err != nil {
     fmt.Printf("Accept error: %s\n", err)
@@ -22,7 +26,19 @@ func main() {
   }
   defer conn.Close()
 
-  var messageBody = "<h1>hoge!!!!!!!!!</h1>"
+  status, err := bufio.NewReader(conn).ReadString('\n')
+  if err != nil {
+    fmt.Printf("bufIO err: %s\n", err)
+    return
+  }
+  //1: method, 2: パス, 3: httpのバージョン
+  splitedStatus := strings.Split(status, " ")
+  path := splitedStatus[1]
+  if(path == "/") {
+    path = "/index.html"
+  }
+  messageBody := readFileContent(path)
+
 	res := GenerateHttpHeader(messageBody)
   res += messageBody + "\n"
 
@@ -41,16 +57,29 @@ func main() {
 	}
 }
 
+func readFileContent(fileName string) string {
+  fp, err := os.Open("./resources" + fileName)
+  if err != nil {
+    return "Not Found!"
+  }
+  scanner := bufio.NewScanner(fp)
+  body := ""
+  for scanner.Scan() {
+    body += scanner.Text()
+  }
+  return body
+}
+
 func CountByteLength(target string)(int) {
   return utf8.RuneCountInString(target)
 }
 
-func GenerateHttpHeader(messageBody string)(string) {
-  var responseStatus = "HTTP/1.1 200 OK\n"
-  var contentType    = "Content-Type: text/html\n"
-  var charset        = "charset=utf-8\n";
-  var serverName     = "Server: goserver\n"
-  var contentLength  = "Content-Length: " + strconv.Itoa(CountByteLength(messageBody) + 1) + "\n"
+func GenerateHttpHeader(messageBody string) string {
+  responseStatus := "HTTP/1.1 200 OK\n"
+  contentType    := "Content-Type: text/html\n"
+  charset        := "charset=utf-8\n";
+  serverName     := "Server: goserver\n"
+  contentLength  := "Content-Length: " + strconv.Itoa(CountByteLength(messageBody) + 1) + "\n"
 
   return responseStatus + contentType + charset + serverName + contentLength + "\n"
 }
